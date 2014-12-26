88bbcd8bf78ec24b873b919dac66f5a845c4f31e
babaoyijia <babaoyijia@163.com>
2014-12-26 23:55:44 +0800

    测试代码同步
    测试代码同步

---
+++ src/com/atguigu/demo/Demo.java
@@ -0,0 +1,13 @@
+package com.atguigu.demo;
+
+public class Demo {
+	public static void main(String[] args) {
+		
+		for(int i=0;i<100;i++){
+			System.out.println("----->>"+i);
+		}
+		System.out.println("Hello World");
+		
+	}
+
+}
